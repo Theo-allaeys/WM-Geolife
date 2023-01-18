cfe98a236de99d804e33218b71b8d573ecca5c7e
Theo-allaeys <theo.allaeys@student.odisee.be>
2023-01-18 15:41:04 +0100

    Update addPOI.vue

--- src/views/addPOI.vue
+++ src/views/addPOI.vue
@@ -7,7 +7,7 @@
                     <ion-img class="logo" src="../../assets/img/logo1024trans.png"></ion-img>
                     <ion-title>GeoLife</ion-title>
                 </div>
-                <input type="file" name="files[]" />
+                <input type="file" name="files[]"/>
                 <ion-button @click="testfile()">Bevestig</ion-button>
             </div>
             </ion-content>
@@ -17,28 +17,71 @@
   
 <script setup>
 import { inject } from 'vue';
+import { Geolocation } from '@capacitor/geolocation';
 const axios = inject('axios')
 function testfile() {
 let file = document.querySelector('[type=file]').files[0];
-console.log(file)
 const formData = new FormData();
 formData.append('files', file);
-console.log(formData.values())
-
+console.log(file.name)
 axios.post('https://theoallaeys2021.be/process.php', formData, {
     headers: {
         'Content-Type': 'multipart/form-data'
     }
 })
     .then((response) => {
-        console.log(response);
+        if (response.status !== 200){
+            console.log(response)
+    }else{
+        console.log(response)
+        addpoi(file.name, "temp")
+    }
     })
     .catch((error) => {
         console.log(error);
-    });
+    })
+
 }
 
+const addpoi = (filename, _name) => {
+let src = "https://theoallaeys2021.be/POI_images/" + filename;
+Geolocation.getCurrentPosition().then((coordinates) => {
+  axios
+    .post('https://theoallaeys2021.be/web&mobile/taak1/api/addPOI.php', {
+      photo: src,
+      name: _name,
+      latitude: coordinates.coords.latitude,
+      longitude: coordinates.coords.longitude
+    })
+    .then(response => {
+      // controleer de response
+      if (response.status !== 200) {
+        // er is iets fout gegaan, doe iets met deze info
+        console.log(response.status);
+      }
+    });
+  });
+};
 
+</script>
+
+<script>
+import { store } from "@/theme/theme";
+import { defineComponent } from "vue";
+import { loggedinstore } from "@/stores/userstore"
+
+export default defineComponent({
+  data() {
+    return {
+      theme: localStorage.getItem("themeSet"),
+      store,
+      loggedinstore,
+      loggedin: localStorage.getItem("loggedinuser"),
+      pseudo: localStorage.getItem("pseudo"),
+      exp: localStorage.getItem("experience")
+    };
+  }
+}); 
 </script>
     
 <style>
